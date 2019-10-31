7ce668fb66a0c39683970b63864eb39f05906d05
Paulemberg Lima Silva <paulemberg@gmail.com>
2019-10-31 09:35:09 -0300

    adding views, pass context, creating templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'authon': 'Berg',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'October 30, 2019'
+    },
+    {
+        'authon': 'Jhon Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 31, 2019'
+    },
+]
+
+
 
 def home(request):
-    return HttpResponse('<h1> Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-     return HttpResponse('<h1> Blog About</h1>') 
+      return render(request, 'blog/about.html')
+
 
 
