30e3ccf2e28450410019ee6b82096a4e165daa57
Paulemberg Lima Silva <paulemberg@gmail.com>
2019-12-04 21:43:13 -0300

    adding migrates, remove dummy data, creating models

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'authon': 'Berg',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'October 30, 2019'
-    },
-    {
-        'authon': 'Jhon Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 31, 2019'
-    },
-]
-
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
